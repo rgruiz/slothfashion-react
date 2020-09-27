d662b38ec5e64a29cad0d456b0e6b769a76e451e
Michel Salinas <michel_2258@hotmail.com>
2020-09-27 18:38:48 -0300

    creacion de componente

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomePage from './pages/HomePage'
 import NewPostPage from './pages/NewPostPage'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Historial from './components/Historial/Historial';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/newpost" component={NewPostPage} />
+          <Route exact path="/historial" component={Historial} />
         </Switch>
       </BrowserRouter>
     );

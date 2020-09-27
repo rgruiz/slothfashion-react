cac7233d34eb8f7eafa448b3a5d0d1cd72d714cf
Rodrigo Ruiz <rodrigo.ruiz@mrm-mccann.com>
2020-09-27 20:42:56 -0300

    pull

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import HomePage from './pages/HomePage'
 import NewPostPage from './pages/NewPostPage'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Publication from './components/Publication/Publication';
+import Historial from './components/Historial/Historial';
 
 class App extends React.Component {
   render() {
@@ -10,6 +12,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/newpost" component={NewPostPage} />
+          <Route exact path="/publication" component={Publication} />
+          <Route exact path="/historial" component={Historial} />
         </Switch>
       </BrowserRouter>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from 'react'
 import Slider from './Slider'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import SearchBar from '../SearchBar/SearchBar';
-import TagsPopulares from '../SearchBar/TagsPopulares';
+import Button from 'react-bootstrap/Button'
+import SearchBar from '../SearchBar/SearchBar'
+import TagsPopulares from '../SearchBar/TagsPopulares'
+import GETMercadoPagoLink from '../DB connections/GETMercadoPagoLink'
 
 class Home extends React.Component {
 
+
+  //esto se usa para cargar el boton pagar con los datos de la compra. el boton dice PAGAR por defecto.........
+    async componentDidMount() {
+
+    const idPost = {idPost: 1}
+
+    const datapreferenceid = await GETMercadoPagoLink(idPost)
+
+    var script = document.createElement('a')
+    var linkText = document.createTextNode("Pagar")
+    script.appendChild(linkText)
+    script.title = "Pagar"
+    script.href = datapreferenceid
+    document.body.appendChild(script)
+    this.div.appendChild(script);
+  }  
 
   render() {
     return (
@@ -21,6 +39,9 @@ class Home extends React.Component {
             <Col xs={6}>
               <TagsPopulares />
             </Col>
+          </Row>
+          <Row>
+            <Button ref={el => (this.div = el)} className="boton-pago"></Button>
           </Row>
         </Container>
       </>

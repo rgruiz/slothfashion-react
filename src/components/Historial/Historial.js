41a14931a493d39d897af98bcc7ede86889060f0
michelsalinas26 <michelsalinas26@gmail.com>
2020-09-28 18:21:26 -0300

    Creación del Componente Historial

--- src/components/Historial/Historial.js
+++ src/components/Historial/Historial.js
@@ -1,21 +1,33 @@
 import React from 'react'
-import {Row, Col, Container} from 'react-bootstrap'
-/*import Publication from '../Publication/Publication';*/
+import { Row, Col, Container } from 'react-bootstrap';
+import Compras from './Compras';
+import Publicaciones from './Publicaciones.js';
+import image from '../../img/header-1.jpg'
+import Style from '../../styles/Publication.css'
+import Select from 'react-select'
+
+
 
 class Historial extends React.Component {
     render() {
-        return(
+        return (
             <Container>
-                <Row xs={12}>
-                    historial
-                </Row>
-                <Row xs={12}>
-                    <col>Publicacion</col>
-                    <col>Compras</col>
-                </Row>
-                <Row>
-                    {/* <Publicacion/> */}
-                </Row>
+                    <Row>
+                        <Col xs={12}>
+                            <Select
+                                name="filtro"
+                                options={[{ value: 'compras', label: 'Ver Compras' }, { value: 'publicaciones', label: 'Ver Publicaciones' }]}
+                                className="basic-multi-select"
+                                classNamePrefix="select"
+                            />
+                        </Col>
+                        <Col xs={12}>
+                            <Compras />
+                        </Col>
+                        <Col xs={12}>
+                            <Publicaciones />
+                        </Col>
+                    </Row>
             </Container>
         )
     }
---
+++ src/components/Historial/Publicaciones.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Row, Table } from 'react-bootstrap';
+import Publication from '../Publication/Publication';
+
+class Publicaciones extends React.Component {
+
+  render() {
+    return (
+      <Row>
+        <h3 className = 'justify-content-center'>Publicaciones</h3>
+          <Table striped bordered hover  className = 'comment'>
+            <tbody>
+              <tr>
+                <td>Publicacion</td>
+              </tr>
+              <tr>
+                <td>Publicacion</td>
+              </tr>
+                <tr>
+                <td>Publicacion</td>
+              </tr>
+                <tr>
+                <td>Publicacion</td>
+              </tr>
+                <tr>
+                <td>Publicacion</td>
+              </tr>
+                <tr>
+                <td>Publicacion</td>
+              </tr>
+            </tbody>
+          </Table>
+      </Row>
+    )
+  }
+}
+
+export default Publicaciones

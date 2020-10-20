4b3d5ca4d394b38b86e7b5b3d3c1e7c513910bb2
gabrielmaceira <gabrielmaceira@gmail.com>
2020-10-20 20:44:36 -0300

    Fix hora comentarios y scroll

--- src/components/Publication/Comment.js
+++ src/components/Publication/Comment.js
@@ -7,32 +7,34 @@ const Comment = props => {
   return (
     <>
       {props.comments.length !== 0 &&
-        <Table striped bordered hover className='comment'>
-          <tbody>
-            {props.comments.map((comment) => <tr>
-              <td>
-                <Row>
-                  <Col>
-                    <Row className="d-flex align-items-center text-muted justify-content-around">
-                      <Col>
-                        <Avatar style={{ background: '#7B1FA2' }}>{comment.nombre.slice(0, 2)}</Avatar> {comment.nombre}
-                      </Col>
-                    </Row>
-                  </Col>
-                  <Col className="d-flex align-items-center text-muted justify-content-around">
-                    {FormatDate(props.comments[0].fechacomentario)}
-                  </Col>
-                </Row>
-                <Row>
-                  <Col className="comment-text mt-2">
-                    {comment.comentario}
-                  </Col>
-                </Row>
-              </td>
-            </tr>
-            )}
-          </tbody>
-        </Table>
+        <div className='comment-container'>
+          <Table striped bordered hover className='comment'>
+            <tbody className='comment-container'>
+              {props.comments.map((comment) => <tr>
+                <td>
+                  <Row>
+                    <Col>
+                      <Row className="d-flex align-items-center text-muted justify-content-around">
+                        <Col>
+                          <Avatar style={{ background: '#7B1FA2' }}>{comment.nombre.slice(0, 2)}</Avatar> {comment.nombre}
+                        </Col>
+                      </Row>
+                    </Col>
+                    <Col className="d-flex align-items-center text-muted justify-content-around">
+                      {FormatDate(comment.fechacomentario)}
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="comment-text mt-2">
+                      {comment.comentario}
+                    </Col>
+                  </Row>
+                </td>
+              </tr>
+              )}
+            </tbody>
+          </Table>
+        </div>
       }
       {props.comments.length === 0 &&
         <Row className='mt-3'>

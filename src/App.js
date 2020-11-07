9d7e3d4313dbf4c2e6ab057fdefd7694a56797d1
gabrielmaceira <gabrielmaceira@gmail.com>
2020-11-07 08:31:42 -0300

    ScrollToTop al cambiar de pagina

--- src/App.js
+++ src/App.js
@@ -9,22 +9,25 @@ import EditPostPage from './pages/EditPostPage'
 import ValidateMPRequestsPage from './pages/ValidateMPRequestsPage'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import ScrollToTop from './ScrollToTop'
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/newpost" component={NewPostPage} />
-          <Route exact path="/publication/:id" component={PublicationPage} />
-          <Route exact path="/historial" component={HistorialPage} />
-          <Route exact path="/search/:tags" component={SearchResultsPage} />
-          <Route exact path="/edit/:id/:user" component={EditPostPage} />
-          <Route exact path="/validateMPRequests" component={ValidateMPRequestsPage} />
-          <Footer></Footer>
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/newpost" component={NewPostPage} />
+            <Route exact path="/publication/:id" component={PublicationPage} />
+            <Route exact path="/historial" component={HistorialPage} />
+            <Route exact path="/search/:tags" component={SearchResultsPage} />
+            <Route exact path="/edit/:id/:user" component={EditPostPage} />
+            <Route exact path="/validateMPRequests" component={ValidateMPRequestsPage} />
+            <Footer></Footer>
+          </Switch>
+        </ScrollToTop>
       </BrowserRouter>
     );
   }

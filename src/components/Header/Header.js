246e30b8a0db23fdb1f0340358367df596c7cb6f
gabrielmaceira <gabrielmaceira@gmail.com>
2020-11-09 09:50:07 -0300

    Cambio estilo NavBar para datos en dos lineas

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Row, Col } from 'react-bootstrap'
 import { Drawer } from 'rsuite'
 import Login from '../Login/Login'
 import SearchBar from '../SearchBar/SearchBar'
@@ -7,7 +7,6 @@ import Cookies from 'universal-cookie'
 import { cookieName } from '../../constants/Cookie'
 import RecuperarCookie from '../Cookies/RecuperarCookie'
 import 'rsuite/dist/styles/rsuite-default.css'; // or 'rsuite/dist/styles/rsuite-default.css'
-import newPost from '../PostEditor/PostForm'
 
 class Header extends React.Component {
 
@@ -53,12 +52,16 @@ class Header extends React.Component {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" bsPrefix="no-margin-top navbar-toggler" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <SearchBar onTagsChange={this.onTagsChange} />
-              <Nav className="ml-auto right-align">
+            <Col xs={12}>
+              <SearchBar onTagsChange={this.onTagsChange} />
+            </Col>
+            <Col xs={12}>
+              <Nav className="mr-auto left-align">
                 {cookie !== undefined && <> <Nav.Link href="/newpost" >Nueva publicación</Nav.Link>
                   <Nav.Link href="/historial" >Ver Historial</Nav.Link></>}
                 {cookie !== undefined ? <Nav.Link onClick={this.clearUserCookies}>Cerrar Sesión</Nav.Link> : <Nav.Link onClick={this.toggleDrawer}>Login</Nav.Link>}
               </Nav>
+            </Col>
           </Navbar.Collapse>
         </Navbar>
 
